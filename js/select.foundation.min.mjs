a59afbc084e40d0f47a2002b918299a589966c27
dtbuild <dtbuild@rigil>
2026-09-25 13:50:36 +0000

    60545c462ae016652eec40667e142a0f2d943a0b Release 4.1.1
    Sync to source repo @60545c462ae016652eec40667e142a0f2d943a0b

--- js/select.foundation.min.mjs
+++ js/select.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! Select Foundation styling 4.1.0 for DataTables
+/*! Select Foundation styling 4.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-zf";import select from"datatables.net-select";export default DataTable;
